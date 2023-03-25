b953d15e506f67831e0aef8a41ce215760be8535
Andrija Pozgaj <apozgaj@symmetria-consulting.hr>
2023-03-25 20:50:20 +0100

    pubsub example

--- Redis.Console/Program.cs
+++ Redis.Console/Program.cs
@@ -9,4 +9,30 @@ var muxer = ConnectionMultiplexer.Connect(options);
 
 var db = muxer.GetDatabase();
 
-Console.WriteLine($"Ping: {db.Ping()}");
+var subscriber = muxer.GetSubscriber();
+var cancellationTokenSource = new CancellationTokenSource();
+var cancellationToken = cancellationTokenSource.Token;
+
+var channel = await subscriber.SubscribeAsync("test-channel");
+
+channel.OnMessage(msg =>
+{
+    Console.WriteLine($"Seq msg received: {msg}, on channel: {msg.Channel}");
+});
+
+await subscriber.SubscribeAsync("test-channel", (channel, msg) =>
+{
+    Console.WriteLine($"Concurrent msg received: {msg}, on channel: {channel}");
+});
+
+Task.Run(async () =>
+{
+    var i = 0;
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        await db.PublishAsync("test-channel", i);
+        await Task.Delay(2000);
+    }
+});
+
+Console.ReadKey();
