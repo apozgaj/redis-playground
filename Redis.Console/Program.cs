d797ac78561c293d0abedcc66953522cf8942ca6
Andrija Pozgaj <apozgaj@symmetria-consulting.hr>
2023-03-25 15:19:44 +0100

    proj setup

---
+++ Redis.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+var options = new ConfigurationOptions
+{
+    EndPoints = new() { "localhost:6379" }
+};
+
+var muxer = ConnectionMultiplexer.Connect(options);
+
+var db = muxer.GetDatabase();
+
+Console.WriteLine($"Ping: {db.Ping()}");
