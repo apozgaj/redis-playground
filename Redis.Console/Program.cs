a02317a05c8baba460d6a171eddd65d4e55d10bb
Andrija Pozgaj <apozgaj@symmetria-consulting.hr>
2023-03-27 08:02:46 +0200

    added sets and hash

--- Redis.Console/Program.cs
+++ Redis.Console/Program.cs
@@ -22,3 +22,36 @@ var numberOfItems = db.ListLeftPush(fruits, new RedisValue[] { "orange", "banana
 var listItem = db.ListGetByIndex(fruits, 0);
 var entireList = string.Join(", ", db.ListRange(fruits));
 Console.WriteLine($"list {entireList}");
+
+
+
+//
+// sets
+//
+
+var allUsers = "users";
+var activeUsers = "users:active";
+var offlineUsers = "users:offline";
+
+db.KeyDelete(new RedisKey[] { allUsers, activeUsers, offlineUsers });
+
+db.SetAdd(activeUsers, new RedisValue[] { "User1", "User2" });
+db.SetAdd(offlineUsers, new RedisValue[] { "User3", "User4" });
+db.SetCombineAndStore(SetOperation.Union, allUsers, new RedisKey[] { activeUsers, offlineUsers });
+
+//
+// hash
+//
+
+var user = "user";
+db.KeyDelete(new RedisKey[] { user });
+
+db.HashSet(user, new HashEntry[]
+{
+    new HashEntry("name", "Andrija"),
+    new HashEntry("age", 30),
+});
+
+var hasgetAll = db.HashGetAll(user);
+
+Console.WriteLine($"hash {string.Join(", ", hasgetAll)}");
